632f0024b32de706505e98ddd7e52b9e33ea7b38
yodeng <yodeng@tju.edu.cn>
2023-04-03 15:16:33 +0800

    update chunksize

--- src/src.py
+++ src/src.py
@@ -13,19 +13,22 @@ class Bopen(object):
 
     def __init__(self, name):
         self.name = name
+        self.chunksize = self.get_chunksize(name)
         self.data = self._cache_data()
 
     def _cache_data(self):
         try:
             dh = StringIO()
             c = b""
-            for chunk in CryptoData.decrypt_file_iter(self.name):
+            for chunk in CryptoData.decrypt_file_iter(self.name, chunksize=self.chunksize):
                 c += chunk
             dh.write(c.decode())
             del c
             dh.seek(0)
-        except:
+        except ValueError:
             dh = open(self.name)
+        except Exception as err:
+            raise err
         return dh
 
     def __enter__(self):
@@ -57,6 +60,13 @@ class Bopen(object):
     def seek0(self):
         self.data.seek(0)
 
-    @classmethod
-    def tobin(cls, intext, outbin):
-        CryptoData.encrypt_file(intext, outbin)
+    @staticmethod
+    def get_chunksize(name):
+        filesize = os.path.getsize(name)
+        size = (filesize // 100) // 16 * 16
+        return max(size, 64 * 1024)
+
+    @staticmethod
+    def tobin(infile, outbin):
+        chunksize = Bopen.get_chunksize(infile)
+        CryptoData.encrypt_file(infile, outbin, chunksize=chunksize)
